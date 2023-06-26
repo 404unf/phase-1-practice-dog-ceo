e24ee07fe02388b842f9a3d6560b3f92cd5c973e
Anna <34191811+404unf@users.noreply.github.com>
2023-06-26 02:04:08 -0700

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+
+// Challenege 1
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+fetch(imgUrl)
+    .then(response=>response.json())
+    .then(data=>data.message.forEach(dog => processData(dog)))
+
+function processData(dog) {
+    const imgContainer = document.getElementById('dog-image-container')
+    const newImage = document.createElement('img')
+    newImage.src = dog
+    imgContainer.append(newImage)
+}
+
+// Challenge 2
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+fetch(breedUrl)
+    .then(response=>response.json())
+    .then(data=>Object.keys(data.message).forEach(breed=>processBreeds(breed)))
+
+function processBreeds(breed) {
+    const ulList = document.getElementById('dog-breeds')
+    const newItem = document.createElement('li')
+    newItem.textContent = breed
+    ulList.append(newItem)
+}
+
+// Challenge 3
